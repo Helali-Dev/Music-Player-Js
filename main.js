cb2ed09867af37570492100014106325b350921d
Fateme Helali <FatemeHelali2005@gmail.com>
2026-08-19 10:56:40 +0330

    feat: add music playlist and track navigation

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const totalTimeDisplay = document.querySelector(".total-time");
 const singerName = document.querySelector(".singer-name");
 const musicName = document.querySelector(".music-name");
 
-// LIST OT OUR MUSIC INFORMATION
+// -------------------------------LIST OT OUR MUSIC INFORMATION
 const playList = [
   {
     singerName: "Saaren",
@@ -35,7 +35,7 @@ const playList = [
 let currentIndex = 0;
 let isPlaying = false;
 
-// LOAD CURRENT MUSIC
+// -------------------------------LOAD CURRENT MUSIC
 const loadAudio = () => {
   const track = playList[currentIndex];
   coverImage.src = track.coverSrc;
@@ -43,12 +43,12 @@ const loadAudio = () => {
   musicName.textContent = track.musicName;
   audio.src = track.audioSrc;
 
-  //   if (isPlaying) {
-  //     audio.play();
-  //   }
+  if (isPlaying) {
+    audio.play();
+  }
 };
 
-// PLAY AND PAUSE MUSIC
+// -------------------------------PLAY AND PAUSE MUSIC
 playPauseButton.addEventListener("click", () => {
   const playPauseIcon = playPauseButton.firstElementChild;
   if (isPlaying) {
@@ -64,15 +64,67 @@ playPauseButton.addEventListener("click", () => {
   }
 });
 
-// HANDLE MUSIC SOUND
+// -------------------------------HANDLE MUSIC SOUND
 volumeSlider.addEventListener("input", () => {
   audio.volume = volumeSlider.value;
 });
 
-// SEEK TO CURRENT PART OD MUSIC
+// -------------------------------SEEK TO CURRENT PART OD MUSIC
 seekSlider.addEventListener("input", () => {
   const newTime = audio.duration * (seekSlider.value / 100);
   audio.currentTime = newTime;
 });
-// LOAD INITIALY MUSIC
+
+// -------------------------------NEXT MUSIC
+nextButton.addEventListener("click", () => {
+  currentIndex++;
+
+  if (currentIndex > playList.length - 1) {
+    currentIndex = 0;
+  }
+  playPauseButton.firstElementChild.classList.replace("fa-play", "fa-pause");
+  isPlaying = true;
+  loadAudio();
+});
+
+// -------------------------------PREVIOUSE MUSIC
+prevButton.addEventListener("click", () => {
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = playList.length - 1;
+  }
+  playPauseButton.firstElementChild.classList.replace("fa-play", "fa-pause");
+  isPlaying = true;
+  loadAudio();
+});
+// -------------------------------
+audio.addEventListener("timeupdate", () => {
+  const currentTIME = audio.currentTime;
+  const totalTime = audio.duration;
+  const percentage = (currentTIME / totalTime) * 100;
+  if (percentage) {
+    seekSlider.value = percentage;
+  }
+
+  if ((currentTIME, totalTime)) {
+    currentTimeDisplay.textContent = formatTime(currentTIME);
+    totalTimeDisplay.textContent = formatTime(totalTime);
+  }
+});
+
+const formatTime = (timeInSeccond) => {
+  const minute = Math.floor(timeInSeccond / 60);
+  const seccound = Math.floor(timeInSeccond % 60);
+
+  return `${minute}:${seccound < 10 ? "0" : ""}${seccound}`;
+};
+
+// RESET MUSIC INFORMATION WHEN AUDIO ENDS
+audio.addEventListener("ended", () => {
+  isPlaying = false;
+  playPauseButton.firstElementChild.classList.replace("fa-pause", "fa-play");
+});
+
+// -------------------------------LOAD INITIALY MUSIC
 loadAudio();
